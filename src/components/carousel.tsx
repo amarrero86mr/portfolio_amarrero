6124cef49ac5e21012c202644e95e9f4066e0c76
amarrero86mr <amarrero86mr@gmail.com>
2025-12-13 23:34:36 -0300

    agregando modal en seccion proyect

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,17 +1,24 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Mousewheel, Keyboard, Autoplay } from 'swiper/modules';
 import { type TSlideProyect } from "../info/proyect_info"
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { DarkLightContext, type TDarkLightContext } from './darklight.context';
+import { ModalProyect } from './modal.proyect';
 
 // eslint-disable-next-line no-alert
 // import "swiper/css"; 
 // import 'swiper/css/navigation';
 // import 'swiper/css/pagination';
 
-export const Carousel = ( props: {data: Array<TSlideProyect>} ) => {
-    const { changeTheme } = useContext<TDarkLightContext>(DarkLightContext)
-    
+export const Carousel = (props: { data: Array<TSlideProyect> }) => {
+  const { changeTheme } = useContext<TDarkLightContext>(DarkLightContext);
+  const [onoffModal, setOnoffModal] = useState<boolean>(false);
+  const [dataModal, setDataModal] = useState<TSlideProyect>()
+
+  const fnOnModalCert = () => {
+    setOnoffModal(!onoffModal)
+  }
+
   return (
     <div className="w-full h-11/12 my-8 p-2">
       <Swiper
@@ -27,21 +34,27 @@ export const Carousel = ( props: {data: Array<TSlideProyect>} ) => {
         }}
         navigation={true}
         pagination={{ clickable: true }}
-        
+
         className={`mySwiper w-full p-4 ${changeTheme}`} >
         {props.data.map((slide) => (
-          <SwiperSlide key={slide.id}className='mb-4'>
-            <div className="m-auto mb-6 flex items-center justify-center gap-6 p-2 rounded-lg shadow-md border-2 border-current max-w-9/12 h-80">
+          <SwiperSlide key={slide.id} className='mb-4'>
+            <div
+              onClick={()=>{setDataModal(slide), fnOnModalCert()}}
+              className="m-auto mb-6 flex items-center justify-center gap-6 p-2 rounded-lg shadow-md border-2 border-current max-w-9/12 h-80">
               {slide.image.trim() !== '' ? <img src={slide.image} alt={slide.title} className="max-w-6/12 m-auto rounded-lg shadow-sm border-2" /> : null}
               <div className='flex flex-col self-start my-4 gap-4'>
-              {slide.title.trim() !== '' ? <h3 className="text-2xl text-center mt-0">{slide.title}</h3> : null}
-              {slide.description.trim() !== '' ? <p className="text-lg content-center mt-4">{slide.description}</p> : null}
+                {slide.title.trim() !== '' ? <h3 className="text-2xl text-center mt-0">{slide.title}</h3> : null}
+                {slide.description.trim() !== '' ? <p className="text-lg content-center mt-4">{slide.description}</p> : null}
 
               </div>
             </div>
           </SwiperSlide>
         ))}
       </Swiper>
+      {onoffModal
+              ? <ModalProyect fnOnModalProyect={fnOnModalCert} data={dataModal!}></ModalProyect>
+              : null
+            }
     </div>
   );
 };
---
+++ src/components/modal.proyect.tsx
@@ -0,0 +1,35 @@
+import { useContext } from "react"
+import type { TSlideProyect } from "../info/proyect_info"
+import { DarkLightContext, type TDarkLightContext } from "./darklight.context"
+
+
+export const ModalProyect = (
+    props: {
+        fnOnModalProyect: () => void,
+        data: TSlideProyect
+    }) => {
+
+    const { changeTheme } = useContext<TDarkLightContext>(DarkLightContext)
+    return (
+        <>
+            <div
+                className={`velo-certificate ${changeTheme}`}
+                onClick={() => props.fnOnModalProyect()}
+            ></div>
+            <div
+                className={`modal-certificate h-8/12 w-9/12 ${changeTheme}`}
+            >
+                <div className="grid grid-cols-6 gap-1">
+                    <h3 className="text-3xl text-center col-span-5">{props.data.title}</h3>
+                    <button
+                        onClick={() => props.fnOnModalProyect()}
+                        className="text-3xl text-right col-span-1 mr-1"
+                    >X</button>
+                </div>
+
+                <img src={props.data.image} alt={props.data.title} className="max-h-10/12 m-auto rounded-lg shadow-sm border-2 place-self-center" />
+
+            </div>
+        </>
+    )
+}
